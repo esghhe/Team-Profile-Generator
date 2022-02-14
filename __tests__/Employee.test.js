1949648f26ae52e4a5dc5b9d7e6bacd6c7881f44
Aden Adhanom <adenadhanom@Adens-MacBook-Air.local>
2022-02-14 14:08:47 -0500

    engineer and intern test files are ready

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,7 +2,7 @@ const Employee = require('../lib/Employee')
 
 describe('Employee Class', () => {
     describe('Initialization', () => {
-        it('Should create an object with an id and name', () =>{
+        it('Should create an object with an id and name', () => {
             const employee = new Employee(123, 'Tim')
 
             expect(employee.id).toEqual(123);
